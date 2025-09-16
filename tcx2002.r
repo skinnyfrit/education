35ea162a365398e1090758504e18c791bf0c8a57
gtyj1998 <gtyj19998@gmail.com>
2025-09-16 14:47:55 +0800

    Added tc2002.r for reference

---
+++ tcx2002.r
@@ -0,0 +1,4 @@
+mixed_numeric_logical <- c(1, 3, 4, TRUE)
+# calling mixed_numeric_logical returns 1 3 4 1
+typeof(mixed_numeric_logical) # returns "double"
+class(mixed_numeric_logical) # returns "numeric"
